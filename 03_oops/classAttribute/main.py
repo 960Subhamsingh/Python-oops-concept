06039a1bf6946d1d22476db466820ebd0bf46c0b
960subhamsingh <kumarsubham0734@gmail.com>
2024-06-28 00:45:24 +0530

    main

--- 03_oops/classAttribute/main.py
+++ 03_oops/classAttribute/main.py
@@ -1,2 +1,14 @@
-class Item:
-    rate = 0.12  # The 
+class A:
+    def __init__(self, value=0):
+        self.a = value
+    def __repr__(self) -> str:
+        return f'<A({self.value})>'
+    def __add__(self, b):
+        return self.a + b
+     
+    def __sub__(self, c):
+        return self.a - c
+
+a = A()
+print(a + 6215 + 2342)
+print(12344-546)
