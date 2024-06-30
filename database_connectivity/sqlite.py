ebbad4d2d147ccd2722c6ccc481a90e2a1103c29
960subhamsingh <kumarsubham0734@gmail.com>
2024-06-30 19:49:52 +0530

    Data analysis with pandas and sqlite

--- database_connectivity/sqlite.py
+++ database_connectivity/sqlite.py
@@ -1,11 +1,38 @@
 import sqlite3
+import pandas as pd
 
-print(sqlite3.sqlite_version)
+data = pd.read_csv('D:/Project/Python_Tutorial/database_connectivity/Data/DimDate.csv')
 
-conn = sqlite3.connect('school.db')
+conn = sqlite3.connect('D:/Project/Python_Tutorial/database_connectivity/school.db')
 
 cur = conn.cursor()
+ 
 
-cur.execute('select * from student')
+dimdat = '''
+DROP TABLE IF EXISTS dimdat;
+
+CREATE TABLE dimdat (
+    DateKey                INTEGER NOT NULL PRIMARY KEY,
+    DateValue              DATE    NOT NULL,
+    DayNumberOfWeek        INTEGER NOT NULL,
+    DayNameOfWeek          TEXT    NOT NULL,
+    DayNumberOfMonth       INTEGER NOT NULL,
+    DayNumberOfYear        INTEGER NOT NULL,
+    WeekNumberOfYear       INTEGER NOT NULL,
+    MonthName              TEXT NOT NULL,
+    MonthNumberOfYear      INTEGER NOT NULL,
+    CalendarQuarter        INTEGER NOT NULL,
+    CalendarYear           INTEGER NOT NULL,
+    FiscalQuarter          INTEGER NOT NULL,
+    FiscalYear             INTEGER NOT NULL 
+);
+''' 
+
+# cur.executescript(dimdat)
+try:
+    print("load data ")
+    data.to_sql('dimat', conn, if_exists='replace', index=False)
+except Exception as e:
+    print('completed data')
 
  
